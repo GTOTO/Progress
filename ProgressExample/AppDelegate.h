f348c157fcd721d0e52119a68a42dac55c4cd35f
“GTOTO” <1138317290@qq.com>
2019-07-21 21:55:01 +0800

    second

---
+++ ProgressExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProgressExample
+//
+//  Created by G on 2019/7/21.
+//  Copyright © 2019 G. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProgressExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProgressExample
+//
+//  Created by G on 2019/7/21.
+//  Copyright © 2019 G. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
